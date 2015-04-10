67e35dec2c450cf9a1ae44be8befc0d7a687b764
Adam Berkovec <ajberk@gmail.com>
2015-04-10 13:45:28 -0700

    seed data is accessible in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "fileutils"
+require_relative('../rawdata/make_seed_data')
+include FileUtils
+cd('../rawdata')
+defense_info = GettingData.get_position_info_for_all_weeks("D.txt")
+kicker_info = GettingData.get_position_info_for_all_weeks("K.txt")
+quarterback_info = GettingData.get_position_info_for_all_weeks("QB.txt")
+runningback_info = GettingData.get_position_info_for_all_weels("RB.txt")
+tightend_info = GettingData.get_position_info_for_all_weels("TE.txt")
+widereciever_info = GettingData.get_position_info_for_all_weels("WR.txt")
