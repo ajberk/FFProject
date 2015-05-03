32f9af5c7934d2c90b83bf4b2f522cbb3a536f13
Adam Berkovec <ajberk@gmail.com>
2015-05-03 14:28:22 -0700

    working on search

---
+++ app/assets/javascripts/collections/search_results.js
@@ -0,0 +1,5 @@
+FFProject.Collections.SearchResults = Backbone.Collection.extend({},{  
+  search: function(query){
+    console.log(query);
+  }
+});
--- app/assets/javascripts/views/positions/search.js
+++ app/assets/javascripts/views/positions/search.js
@@ -1,8 +1,20 @@
 FFProject.Views.SearchForm = Backbone.View.extend({
   template: JST["positions/search"],
 
+  events: {
+    "submit form.search": "submit",
+  },
+
   render: function(){
     this.$el.html(this.template());
     return this;
   },
+
+  submit: function(event) {
+    event.preventDefault();
+    var $target = $(event.currentTarget);
+    debugger
+    data = $target.serialize()
+    alert(data)
+  }
 });
