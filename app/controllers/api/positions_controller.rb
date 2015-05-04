f21c898c257b30a2ad945ea0f4f0ae0d8e9eaa7b
Scott Nelson <Scott.D.Nelson@dartmouth.edu>
2015-05-04 14:13:43 -0700

    apply scope to position, test with one week

--- app/controllers/api/positions_controller.rb
+++ app/controllers/api/positions_controller.rb
@@ -1,8 +1,17 @@
 class Api::PositionsController < ApplicationController
-  has_scope :id
+  has_scope :opponent
 
   def index
-    @positions = apply_scopes(Position).limit(10)
+    # @positions = apply_scopes(Position).where(id: 51)
+    @positions = apply_scopes(Position).where(week: 1)
+    # week = 2
+    # while week < 4
+    #   max = @positions.where(week: week).maximum(:points)
+    #   @positions.merge(apply_scopes(Position).where(week: week))
+    #   week+=1
+    # end
+
+    @positions
   end
 
   def show
